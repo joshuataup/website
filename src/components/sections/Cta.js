83e512959b4b160090ea60b8757edf005ff5827b
Christine Baba <17974025+christin3@users.noreply.github.com>
2021-06-15 20:39:06 -0500

    contact email

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -57,11 +57,7 @@ const Cta = ({
               </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+            <h4><a href="mailto:anthony@hypebility.com?subject=I'm interested in Hypebility!">Click here to Email Us</a></h4>
           </div>
         </div>
       </div>
